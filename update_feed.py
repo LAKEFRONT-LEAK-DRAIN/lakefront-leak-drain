8f7f7e630f8c6b6a889db321e54c8bbb59794ede
lakefrontleakanddrain-design <265939516+lakefrontleakanddrain-design@users.noreply.github.com>
2026-03-31 01:09:13 +0000

    Fix: Character Scrubber Enabled

--- update_feed.py
+++ update_feed.py
@@ -31,45 +31,15 @@ xml_prompt = f"Write a valid RSS <item> block for a post titled '{title}'. Use '
 final_resp = client.models.generate_content(model='gemini-2.5-flash', contents=xml_prompt)
 new_item = final_resp.text.strip().replace('```xml', '').replace('```', '').strip()
 
-# SCRUBBER: This fixes the 'EntityRef' error by encoding all ampersands properly
-# We do this twice to ensure we don't 'double-encode' existing entities
-new_item = new_item.replace('&amp;', '&') 
-new_item = new_item.replace('&', '&amp;')
+# SCRUBBER: This replaces bad characters that crash XML
+new_item = new_item.replace('& ', '&amp; ').replace(' &', ' &amp;')
 
-# 4. Inject (PREPEND: Forces Newest to the Top for Metricool)
+# 4. Inject
 with open('feed.xml', 'r', encoding='utf-8') as f:
     feed = f.read()
 
-# We look for the <language> tag specifically. 
-# We use .lower() to make sure we don't miss it due to capitalization.
-marker = '</language>'
-insert_pos = feed.lower().find(marker.lower())
-
+insert_pos = feed.rfind('</channel>')
 if insert_pos != -1:
-    # Add the length of the tag to move past it
-    insert_pos += len(marker)
-    
-    # Build the feed: Metadata + New Post + Existing Posts
-    updated_feed = feed[:insert_pos] + '\n\n    ' + new_item + feed[insert_pos:]
-    
+    updated_feed = feed[:insert_pos] + '    ' + new_item + '\n\n' + feed[insert_pos:]
     with open('feed.xml', 'w', encoding='utf-8') as f:
         f.write(updated_feed)
-    print(f"Success! New post '{title}' inserted at the top.")
-else:
-    # If </language> is missing, we look for the end of the channel header
-    fallback_marker = '<channel>'
-    fallback_pos = feed.lower().find(fallback_marker.lower())
-    
-    if fallback_pos != -1:
-        insert_pos = fallback_pos + len(fallback_marker)
-        updated_feed = feed[:insert_pos] + '\n\n    ' + new_item + feed[insert_pos:]
-        with open('feed.xml', 'w', encoding='utf-8') as f:
-            f.write(updated_feed)
-        print("Used fallback insertion point near top.")
-    else:
-        # Final emergency fallback: Append to bottom if the file is totally weird
-        print("Warning: Could not find any valid markers. Appending to bottom.")
-        insert_pos = feed.rfind('</channel>')
-        updated_feed = feed[:insert_pos] + '    ' + new_item + '\n' + feed[insert_pos:]
-        with open('feed.xml', 'w', encoding='utf-8') as f:
-            f.write(updated_feed)
