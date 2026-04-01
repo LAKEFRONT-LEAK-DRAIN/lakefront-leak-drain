461917d7ce09bb3efed3931dfc22437451874c1f
lakefrontleakanddrain-design <lakefrontleakanddrain@gmail.com>
2026-04-01 08:23:43 -0400

    Update update_feed.py

--- update_feed.py
+++ update_feed.py
@@ -33,7 +33,7 @@ def get_image_length(image_url):
 def create_slug(title):
     """Turns 'Spring Sump Pump!' into 'spring-sump-pump'"""
     slug = title.lower()
-    slug = re.sub(r'[^a-z0-9 ]', '', slug) 
+    slug = re.sub(r'[^a-z0-9 ]', '', slug)  # Removes special chars
     slug = slug.strip().replace(' ', '-')
     return slug
 
